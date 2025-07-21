59b8b6644b581f9e7ba82358dddf2b9d510d4276
jmpark <jmpark@atoncorp.com>
2025-07-21 17:48:27 +0900

    feat: 포스트 수정삭제 버튼 추가

--- client/src/app/page/post/post.component.ts
+++ client/src/app/page/post/post.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { HeaderLayout } from "../../component/header/header.component";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-post',
@@ -12,7 +12,7 @@ export class PostPage implements OnInit, OnDestroy {
   blogId: string | null;
   postId: string | null;
   data: any;
-  constructor(private route: ActivatedRoute) {
+  constructor(private router: Router, private route: ActivatedRoute) {
     this.blogId = route.snapshot.paramMap.get('blogId');
     this.postId = route.snapshot.paramMap.get('postId');
 
@@ -43,5 +43,24 @@ export class PostPage implements OnInit, OnDestroy {
 
     const json = await response.json();
     this.data = json?.[0];
+  }
+
+  onSave = async () => {
+    this.router.navigate([`/edit/${this.postId}`]);
+  }
+
+  onDelete = async () => {
+    const response = await fetch(
+      `http://localhost:3000/api/posts?_id=${this.postId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          "Authentication": "Bearer "
+        }
+      }
+    )
+
+    const json = await response.json();
   }
 }
