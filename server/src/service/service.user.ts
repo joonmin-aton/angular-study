6af56564d1ac52d1e7de8b85b1e501274dbd801e
jmpark <jmpark@atoncorp.com>
2025-07-23 14:59:17 +0900

    fix: [Server]

--- server/src/service/service.user.ts
+++ server/src/service/service.user.ts
@@ -6,6 +6,9 @@ const info = async (req: any, res: any) => {
     try {
         const token = req.headers?.authorization?.replace("Bearer ", "");
         const claims: any = jwt.verify(token, JWT_SECRET_KEY, { algorithms: ["HS256"]});
+        if (!claims) {
+            res.status(400).json("인증 실패되었습니다");
+        }
         const exist = await userSchema.findOne({ _id: claims.id });
         res.status(200).json(exist);
     } catch (err) {
